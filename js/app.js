9e66c286038d37bad15dd6759dd6cfeac697f582
jahangirhusseen <jahangirhusseenid@gmail.com>
2022-03-06 20:26:41 +0600

    Upload all project Files

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+
+// let food = ['alo','potol','kumra','tomato','derots'];
+// console.log(food);
+// console.log(food.splice(2,1,'mula'));
+// console.log(food);
+// console.log(food.slice(0,2));
+// console.log(food);
+
+
+// let s = 'do';
+// let n = 2;
+// let un;
+// let nu = null;
+// console.log(typeof s);
+// console.log(typeof n);
+// console.log(typeof food);
+// console.log(typeof un);
+// console.log(nu);
+
+
+
+// let date = new Date();
+
+// console.log((date.getDate() < 9) ? date.getDate() : '00'+ date.getDate() );
+ 
+// let day = function (index){
+//     let dayname = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+//     return dayname [index];
+// }
+
+
+
+// console.log(day(date.getDay()));
+let date = new Date();
+console.log(date);
+console.log(Math.E);
